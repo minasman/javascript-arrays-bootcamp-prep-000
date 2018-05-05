dd74eb80df3d307a91c71b27aa8fd19fa18b1ceb
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-05 01:14:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,41 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, element) {
+  var newArr = [element, ...arr];
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  arr = arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, element){
+  var newArr = [...arr, element];
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray(arr, element){
+  arr = arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr, index){
+  return arr.indexOf(index);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  return arr.shift();
+}
+
+function removeElementFromBeginningOfArray(arr){
+  var newArr = arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr){
+  return arr.pop();
+}
+
+function removeElementFromEndOfArray(arr){
+  var newArr = arr.slice(0, arr.length-1);
+}
